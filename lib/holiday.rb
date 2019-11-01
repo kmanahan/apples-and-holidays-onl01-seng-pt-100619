0a6701c141a354b15ae6f9f6d24dd325a1d08b91
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-01 17:49:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash do |holiday, season|
+  holiday_hash/each do |holiday, season|
     puts season + ":"
 
 end
